cf1d338d44e0ae83a80f62dcb7d06c89c18640fe
Kanan Kotwani <kanankotwani28@gmail.com>
2026-05-19 03:07:38 +0530

    chore: add allow_origin_regex to support all vercel subdomains and local ports

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,6 +39,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "https://atomquest-assignment.vercel.app",
     ],  # Vite frontend
+    allow_origin_regex=r"https://.*\.vercel\.app|http://localhost:\d+|http://127.0.0.1:\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
