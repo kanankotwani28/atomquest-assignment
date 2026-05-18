abd5e20adcf8e882b52b63d62cfdb3916c1736b9
Kanan Kotwani <kanankotwani28@gmail.com>
2026-05-19 02:13:28 +0530

    feat: initialize FastAPI application with database migrations, routers, and background services

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Vite frontend
+    allow_origins=["https://atomquest-assignment.vercel.app/"],  # Vite frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
